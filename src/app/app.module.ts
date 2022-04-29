ef8b421b8fedb12477e027919eaef1d28e645824
leandroatavares <leandroanjostavares@gmail.com>
2022-04-29 12:19:05 -0300

    adiciona form e burron modules (material)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -35,7 +37,9 @@ registerLocaleData(localePt, 'pt');
     BrowserAnimationsModule,
     FontAwesomeModule,
     MatSidenavModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
