3678c5766b03e27dab5f5023cbb5a5937adc8165
leandroatavares <leandroanjostavares@gmail.com>
2022-04-28 21:12:53 -0300

    adiciona reactive forms e form de categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import localePt from '@angular/common/locales/pt';
 import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -9,10 +10,11 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntriesTableComponent } from './components/entries-table/entries-table.component';
-import { SummaryComponent } from './components/summary/summary.component';
-import { HomeComponent } from './pages/home/home.component';
+import { FormCategoryComponent } from './components/form-category/form-category.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { SummaryComponent } from './components/summary/summary.component';
 import { CategoryComponent } from './pages/category/category.component';
+import { HomeComponent } from './pages/home/home.component';
 
 registerLocaleData(localePt, 'pt');
 
@@ -23,7 +25,8 @@ registerLocaleData(localePt, 'pt');
     EntriesTableComponent,
     HomeComponent,
     SidenavComponent,
-    CategoryComponent
+    CategoryComponent,
+    FormCategoryComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +34,8 @@ registerLocaleData(localePt, 'pt');
     HttpClientModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
-    MatSidenavModule
+    MatSidenavModule,
+    ReactiveFormsModule
   ],
   providers: [
     { provide: LOCALE_ID, useValue: 'pt' },
